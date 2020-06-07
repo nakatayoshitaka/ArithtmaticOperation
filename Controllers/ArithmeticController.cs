8479923bab2af3fede2553b8d4072feb36bf8bc3
yn_01 <yn_01@LAPTOP-DASOPJEJ>
2020-06-07 18:51:57 +0900

    ArithmaticApplication

---
+++ Controllers/ArithmeticController.cs
@@ -0,0 +1,30 @@
+﻿using ArithmeticCalculation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArithmeticCalculation.Controllers
+{
+    public class ArithmeticController : Controller
+    {
+        // GET: Arithmetic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Default model,string Operater)
+        {            
+            if (Operater == "add") model.Result = model.Value1 + model.Value2;
+            if (Operater == "sub") model.Result = model.Value1 - model.Value2;
+            if (Operater == "mul") model.Result = model.Value1 * model.Value2;
+            if (Operater == "div") model.Result = model.Value1 / model.Value2;
+                        
+            ModelState.Clear();
+            return View(model);
+        }
+    }
+}
---
+++ Models/Default.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArithmeticCalculation.Models
+{
+    public class Default
+    {
+        public double Value1 { get; set; }
+        public double Value2 { get; set; }
+        public double Result { get; set; }
+    }
+}
